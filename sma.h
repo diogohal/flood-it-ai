54020868c6ad54c35dca0ec31d60b5975e515cd1
nataelpg <91358498+nataelpg@users.noreply.github.com>
2023-05-14 22:41:29 -0300

    Update sma.h

--- sma.h
+++ sma.h
@@ -1,20 +1,30 @@
 #ifndef SMA_H
 #define SMA_H
+#include "struct.h"
 
 typedef struct node {
 
     int weight;
     int color;
-    int *colors;
+    int colors[5];
+    int corner;
     struct node **children;
 
 } node_t;
 
 typedef struct root {
 
-    slot_t *slots[4];
-    struct node *father;
+    int numColored;
+    struct node *init;
 
 } root_t;
+
+int calculate_weight(node_t *node, int numColors);
+node_t *create_node(int numColors);
+root_t* create_root(int m, int n, int numColors);
+node_t *expand_node(board_t *board, node_t *node, int m, int n, int numColors);
+node_t *decision(node_t *node, int numColors);
+void printChildren(node_t *node);
+void destroy_root(root_t *root, int numColors);
 
 #endif
