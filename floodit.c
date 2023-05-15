ff6ffa2360d9cb71c4cd348e9a005897c8cdc254
nataelpg <91358498+nataelpg@users.noreply.github.com>
2023-05-14 22:40:49 -0300

    Update floodit.c

--- floodit.c
+++ floodit.c
@@ -1,32 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 #include "struct.h"
 #include "sma.h"
-#include <time.h>
+
+int calcula_maior(int *colors, int numColors) {
+    int maior = 0;
+    for(int i = 0; i < numColors; i++) {
+        if(colors[i] > colors[maior])
+            maior = i;
+    }
+    return maior;
+}
 
 int main() {
 
-    int m, n, numColors, numMoves = 0;
+    // Creates important structures and variables
+    int m, n, color, numColors, numMoves, numNonColored = 0;
     scanf("%d %d %d", &m, &n, &numColors);
     printf("%d %d %d\n", m, n, numColors);
     board_t *board = create_board(m, n, numColors);
+    root_t *root = create_root(m, n, numColors);
+    node_t* node = malloc(sizeof(node_t));
 
+
+    int maior = 0;
+    srand(time(NULL));
 
     while (!is_board_colored(board, m, n)) {
-        // int color;
-        // //imprime as opções de cores coloridas   
-        // printf("Escolha uma cor:\n");
-        // for (int i = 0; i < numColors; i++) {
-        //     print_slot(i);
-        //     printf(" %d ", i); 
-        // }
+        // imprime as opções de cores coloridas   
+        printf("Escolha uma cor:\n");
+        for (int i = 0; i < numColors; i++) {
+            print_slot(i);
+            printf(" %d ", i); 
+        }
         printf("\n\n");
         print_board(board, m, n);
         // scanf("%d", &color);
+        // expand_node(board, node, m, n, numColors);
+        // printChildren(node);
+        // node = decision(node, numColors);
+        calculate_weight3(board, board->slots[0][0] , 0, node);
+        calculate_weight3(board, board->slots[0][0] , 1, node);
+        calculate_weight3(board, board->slots[0][0] , 2, node);
+        calculate_weight3(board, board->slots[0][0] , 3, node);
+        calculate_weight3(board, board->slots[0][0] , 4, node);
+
+        for (int i = 0; i < numColors; i++) {
+            printf(" %d ", node->colors[i]); 
+        }
+
+        maior = calcula_maior(node->colors, numColors);
+        printf("Maior = %d\n", maior);
+        sleep(1);
         numMoves++;
-        //color = HEURISTICA
-        flood_fill(board, m, n, color);
+        //color = 
+    
+        flood_fill(board, m, n, maior, node->corner);
+        numNonColored = countNonColored(board, m, n);
+        print_slot(node->color);
+        printf("ESCOLHA = Cor: %d | Canto: %d | Peso: %d | Restante = %d\n", node->color, node->corner, node->weight, numNonColored);
     }
+    print_board(board, m, n);
+    printf("Parabéns, você completou o tabuleiro! Número de passos: %d\n", numMoves);
+
+    destroy_root(root, numColors);
     destroy_board(board, m, n);
     return 0;
 
